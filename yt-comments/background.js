ee6831dcfc1e3b46bda6275db7c4f47e4b25b772
Jesh <pigihunter@gmail.com>
2024-07-25 17:44:53 -0600

    Idk what draft number it is for yt extension, but it does what it needs to do. Also, focus-mode was a tutorial

---
+++ yt-comments/background.js
@@ -0,0 +1,31 @@
+chrome.runtime.onInstalled.addListener( () => {
+    chrome.action.setBadgeText({
+        text: 'OFF',
+    });
+});
+
+const video_link = 'https://www.youtube.com/watch?v=';
+
+chrome.action.onClicked.addListener( async (tab) => {
+    if ( tab.url.startsWith( video_link )) {
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        const nextState = ( prevState === 'ON') ? 'OFF' : 'ON'
+        
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
+        });
+        
+        if ( nextState === 'ON' ) {
+            await chrome.scripting.insertCSS({
+                files: ["css/remove-comments.css"],
+                target: { tabId: tab.id },
+            });
+        } else if ( nextState === 'OFF' ) {
+            await chrome.scripting.removeCSS({
+                files: ["css/remove-comments.css"],
+                target: { tabId: tab.id },
+            });
+        }
+    }
+});
